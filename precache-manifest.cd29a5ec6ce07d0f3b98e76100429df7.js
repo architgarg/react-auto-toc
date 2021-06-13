86edb41f9dceb85de134d8586c8c526dff83ab6c
Archit Garg <thearchitgargg@gmail.com>
2021-06-13 23:34:41 +0530

    Updates

---
+++ precache-manifest.cd29a5ec6ce07d0f3b98e76100429df7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a47869bf98b66295280354227c761d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc104d2f2edaa9a6fa14",
+    "url": "./static/css/main.65a88fdb.chunk.css"
+  },
+  {
+    "revision": "9ac7e7896195c563364c",
+    "url": "./static/js/2.69d05679.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.69d05679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc104d2f2edaa9a6fa14",
+    "url": "./static/js/main.b23d724b.chunk.js"
+  },
+  {
+    "revision": "11ddb26551b7d26c0b624912c9ffb006",
+    "url": "./static/js/main.b23d724b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2783896f4ace754bbd1e",
+    "url": "./static/js/runtime-main.e72d33b7.js"
+  }
+]);
